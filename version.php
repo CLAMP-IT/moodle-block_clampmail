4d6c22fb07f6d5a2623fa5eb33b7984b6b18a4d8
Charles Fulton <fultonc@lafayette.edu>
2019-01-08 22:21:17 -0500

    CLAMPMail v3.3.5

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018060100; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2019010800; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017051500; // Requires this Moodle version.
 $plugin->component = 'block_clampmail'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE; // The current stability of this version (used for diagnostics).
-$plugin->release   = 'v3.3.4'; // The release name of this version (used for diagnostics).
+$plugin->release   = 'v3.3.5'; // The release name of this version (used for diagnostics).
